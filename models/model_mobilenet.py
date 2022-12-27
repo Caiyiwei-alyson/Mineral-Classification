b64f650727c8ef1b60f79b0bcbc804c7b41542d1
Caiyiwei-alyson <118888642+Caiyiwei-alyson@users.noreply.github.com>
2022-12-27 11:57:03 +0800

    Update model_mobilenet.py

--- models/model_mobilenet.py
+++ models/model_mobilenet.py
@@ -13,104 +13,119 @@ def _make_divisible(ch, divisor=8, min_ch=None):
         min_ch = divisor
     new_ch = max(min_ch, int(ch + divisor / 2) // divisor * divisor)
     # Make sure that round down does not go down by more than 10%.
-    if new_ch < 0.9 * ch:
+    if new_ch < 0.9 * v:
         new_ch += divisor
-    return new_ch
+    return new_v
 
 
-class ConvBNReLU(nn.Sequential):
-    def __init__(self, in_channel, out_channel, kernel_size=3, stride=1, groups=1):
-        padding = (kernel_size - 1) // 2
-        super(ConvBNReLU, self).__init__(
-            nn.Conv2d(in_channel, out_channel, kernel_size, stride, padding, groups=groups, bias=False),
-            nn.BatchNorm2d(out_channel),
-            nn.ReLU6(inplace=True)
-        )
+def conv_3x3_bn(inp, oup, stride):
+    return nn.Sequential(
+        nn.Conv2d(inp, oup, 3, stride, 1, bias=False),
+        nn.BatchNorm2d(oup),
+        nn.ReLU6(inplace=True)
+    )
 
+def conv_1x1_bn(inp, oup):
+    return nn.Sequential(
+        nn.Conv2d(inp, oup, 1, 1, 0, bias=False),
+        nn.BatchNorm2d(oup),
+        nn.ReLU6(inplace=True)
+    )
 
 class InvertedResidual(nn.Module):
-    def __init__(self, in_channel, out_channel, stride, expand_ratio):
+    def __init__(self, inp, oup, stride, expand_ratio):
         super(InvertedResidual, self).__init__()
-        hidden_channel = in_channel * expand_ratio
-        self.use_shortcut = stride == 1 and in_channel == out_channel
+        assert stride in [1, 2]
+        
+        hidden_dim = round(inp * expand_ratio)
+        self.identity = stride == 1 and inp == oup
 
-        layers = []
-        if expand_ratio != 1:
-            # 1x1 pointwise conv
-            layers.append(ConvBNReLU(in_channel, hidden_channel, kernel_size=1))
-        layers.extend([
-            # 3x3 depthwise conv
-            ConvBNReLU(hidden_channel, hidden_channel, stride=stride, groups=hidden_channel),
-            # 1x1 pointwise conv(linear)
-            nn.Conv2d(hidden_channel, out_channel, kernel_size=1, bias=False),
-            nn.BatchNorm2d(out_channel),
-        ])
-
-        self.conv = nn.Sequential(*layers)
+        if expand_ratio == 1:
+            self.conv = nn.Sequential(
+                # dw
+                nn.Conv2d(hidden_dim, hidden_dim, 3, stride, 1, groups=hidden_dim, bias=False),
+                nn.BatchNorm2d(hidden_dim),
+                nn.ReLU6(inplace=True),
+                # pw-linear
+                nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
+                nn.BatchNorm2d(oup),
+            )
+        else:
+            self.conv = nn.Sequential(
+                # pw
+                nn.Conv2d(inp, hidden_dim, 1, 1, 0, bias=False),
+                nn.BatchNorm2d(hidden_dim),
+                nn.ReLU6(inplace=True),
+                # dw
+                nn.Conv2d(hidden_dim, hidden_dim, 3, stride, 1, groups=hidden_dim, bias=False),
+                nn.BatchNorm2d(hidden_dim),
+                nn.ReLU6(inplace=True),
+                # pw-linear
+                nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
+                nn.BatchNorm2d(oup),
+            )
 
     def forward(self, x):
-        if self.use_shortcut:
+        if self.identity:
             return x + self.conv(x)
         else:
-            return self.conv(x)
-
-
+            return self.conv(x)        
+ 
 class MobileNetV2(nn.Module):
-    def __init__(self, num_classes=1000, alpha=1.0, round_nearest=8):
+    def __init__(self, num_classes=1000, width_mult=1.):
         super(MobileNetV2, self).__init__()
-        block = InvertedResidual
-        input_channel = _make_divisible(32 * alpha, round_nearest)
-        last_channel = _make_divisible(1280 * alpha, round_nearest)
-
-        inverted_residual_setting = [
+        # setting of inverted residual blocks
+        self.cfgs = [
             # t, c, n, s
-            [1, 16, 1, 1],
-            [6, 24, 2, 2],
-            [6, 32, 3, 2],
-            [6, 64, 4, 2],
-            [6, 96, 3, 1],
+            [1,  16, 1, 1],
+            [6,  24, 2, 2],
+            [6,  32, 3, 2],
+            [6,  64, 4, 2],
+            [6,  96, 3, 1],
             [6, 160, 3, 2],
             [6, 320, 1, 1],
         ]
 
-        features = []
-        # conv1 layer
-        features.append(ConvBNReLU(3, input_channel, stride=2))
-        # building inverted residual residual blockes
-        for t, c, n, s in inverted_residual_setting:
-            output_channel = _make_divisible(c * alpha, round_nearest)
+        # building first layer
+        input_channel = _make_divisible(32 * width_mult, 4 if width_mult == 0.1 else 8)
+        layers = [conv_3x3_bn(3, input_channel, 2)]
+        # building inverted residual blocks
+        block = InvertedResidual
+        for t, c, n, s in self.cfgs:
+            output_channel = _make_divisible(c * width_mult, 4 if width_mult == 0.1 else 8)
             for i in range(n):
-                stride = s if i == 0 else 1
-                features.append(block(input_channel, output_channel, stride, expand_ratio=t))
+                # import pdb
+                # pdb.set_trace()
+                layers.append(block(input_channel, output_channel, s if i == 0 else 1, t))
                 input_channel = output_channel
+        self.features = nn.Sequential(*layers)
         # building last several layers
-        features.append(ConvBNReLU(input_channel, last_channel, 1))
-        # combine feature layers
-        self.features = nn.Sequential(*features)
-
-        # building classifier
+        output_channel = _make_divisible(1280 * width_mult, 4 if width_mult == 0.1 else 8) if width_mult > 1.0 else 1280
+        self.conv = conv_1x1_bn(input_channel, output_channel)
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.classifier = nn.Sequential(
-            nn.Dropout(0.2),
-            nn.Linear(last_channel, num_classes)
-        )
+        self.classifier = nn.Linear(output_channel, num_classes)
 
-        # weight initialization
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out')
-                if m.bias is not None:
-                    nn.init.zeros_(m.bias)
-            elif isinstance(m, nn.BatchNorm2d):
-                nn.init.ones_(m.weight)
-                nn.init.zeros_(m.bias)
-            elif isinstance(m, nn.Linear):
-                nn.init.normal_(m.weight, 0, 0.01)
-                nn.init.zeros_(m.bias)
+        self._initialize_weights()
 
     def forward(self, x):
         x = self.features(x)
+        x = self.conv(x)
         x = self.avgpool(x)
-        x = torch.flatten(x, 1)
+        x = x.view(x.size(0), -1)
         x = self.classifier(x)
         return x
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                m.weight.data.normal_(0, 0.01)
+                m.bias.data.zero_()
+
